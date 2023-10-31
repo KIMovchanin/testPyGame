523f9b6e3db41434f3be0049d9c95ee9f7750844
KIMovchanin <kondr2004k@gmail.com>
2023-10-31 13:45:51 +0300

    3 add image, font and text

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pygame
 # обазетльный метод в начале для инициализации игры
 pygame.init()
 # указываем размер экрана в формате (ширина, высота)
-screen = pygame.display.set_mode((600, 300))  # третий параметр говорит нам о том как запустить приложение.
+screen = pygame.display.set_mode((1000, 700))  # третий параметр говорит нам о том как запустить приложение.
                                               # Указав flags=pygame.NOFRAME не будет кнопок закрытия/сворачивания
 # добавляем название для приложения
 pygame.display.set_caption("PyGame Ijinerium")
@@ -12,11 +12,27 @@ icon = pygame.image.load('Materials/inj.jpg')
 # утсанавливаю картинку в качестве иконки
 pygame.display.set_icon(icon)
 
+# создаём объект square рисуя через класс Surface() квадрат
+square = pygame.Surface((50, 170))   # принимает в качестве параметра кортеж со значениями ширины и высоты
+square.fill((50, 170, 80))
+
+# создаём надпись
+myfont = pygame.font.Font('Materials/Fonts/Agbalumo-Regular.ttf', 40)  # первым параметром получает шрифт, вторым - размер шрифта
+# дополнительные характеристики
+text_surface = myfont.render('Injinerium', False, 'Yellow')    # мы можем указать здесь сам текст, что будет отображён, цвет, задний фон и тд
+# подгружаю картинку
+image = pygame.image.load('C:/Users/kondr/OneDrive/Рабочий стол/Арты/hThxjaFdCbQ.jpg')
+
 running = True
 while running:
-    # меняем фон игры
-    # screen.fill((50, 170, 80))   # в качестве параметра мы передаём кортеж
 
+    # рисуем на экране наш квадрат с помощью метода blit
+    screen.blit(square, (0, 0))    # принимает два параметра, первый - сам объект. Второй - координаты спавна
+    screen.blit(text_surface, (200, 200))
+    # другой способ рисования в 1 строку кода
+    # рисуем объект круг на поверхности "screen", выбираю цвет, кортежем передаю координаты и задаю радиус круга
+    pygame.draw.circle(screen, 'Red', (100, 40), 30)
+    screen.blit(image, (400, 0))
     # обновление экрана
     pygame.display.update()
 
@@ -25,8 +41,6 @@ while running:
         if event.type == pygame.QUIT:   # если событие это нажатие на выход
             running = False
             pygame.quit()               # то игры закроется. После этого действия никакие другие не должны быть
-        elif event.type == pygame.KEYDOWN:  # если была нажата клавиша
-            if event.key == pygame.K_SPACE: # то проверяем какая клавиша
-                screen.fill((170, 50, 140)) # и меняем цвет в зависимости от нажатой клавиши
-            elif event.key == pygame.K_LEFT:
-                screen.fill((50, 170, 80))
+
+
+
