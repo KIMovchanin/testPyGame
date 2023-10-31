4bbc622c944f1938bda8ddb5cf1852390966cb7f
KIMovchanin <kondr2004k@gmail.com>
2023-10-31 13:21:53 +0300

    2 change backgroun, icon and name of program

--- main.py
+++ main.py
@@ -3,10 +3,19 @@ import pygame
 # обазетльный метод в начале для инициализации игры
 pygame.init()
 # указываем размер экрана в формате (ширина, высота)
-screen = pygame.display.set_mode((600, 300))
+screen = pygame.display.set_mode((600, 300))  # третий параметр говорит нам о том как запустить приложение.
+                                              # Указав flags=pygame.NOFRAME не будет кнопок закрытия/сворачивания
+# добавляем название для приложения
+pygame.display.set_caption("PyGame Ijinerium")
+# подгружаю картинку в проект
+icon = pygame.image.load('Materials/inj.jpg')
+# утсанавливаю картинку в качестве иконки
+pygame.display.set_icon(icon)
 
 running = True
 while running:
+    # меняем фон игры
+    # screen.fill((50, 170, 80))   # в качестве параметра мы передаём кортеж
 
     # обновление экрана
     pygame.display.update()
@@ -16,3 +25,8 @@ while running:
         if event.type == pygame.QUIT:   # если событие это нажатие на выход
             running = False
             pygame.quit()               # то игры закроется. После этого действия никакие другие не должны быть
+        elif event.type == pygame.KEYDOWN:  # если была нажата клавиша
+            if event.key == pygame.K_SPACE: # то проверяем какая клавиша
+                screen.fill((170, 50, 140)) # и меняем цвет в зависимости от нажатой клавиши
+            elif event.key == pygame.K_LEFT:
+                screen.fill((50, 170, 80))
