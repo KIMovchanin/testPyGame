6824e951814d1cb130060e00c8bf489d10f3f737
KIMovchanin <kondr2004k@gmail.com>
2023-10-31 13:08:23 +0300

    1 init without errors

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print('hi')
+import pygame
+
+# обазетльный метод в начале для инициализации игры
+pygame.init()
+# указываем размер экрана в формате (ширина, высота)
+screen = pygame.display.set_mode((600, 300))
+
+running = True
+while running:
+
+    # обновление экрана
+    pygame.display.update()
+
+    # чтобы закрыть экран
+    for event in pygame.event.get():    # за счёт метода pygame.event.get() мы получаем список из всех возможных событий
+        if event.type == pygame.QUIT:   # если событие это нажатие на выход
+            running = False
+            pygame.quit()               # то игры закроется. После этого действия никакие другие не должны быть
